a369d88a0311719803e0b4297535a3e3eafc9330
raduubraduu <lefter_radu@yahoo.com>
2023-08-31 19:03:37 +0100

    github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://radu_lefter.github.io',
+    base: '/cursuri-seo',
     integrations: [tailwind({
         //applyBaseStyles: false,
     })]
